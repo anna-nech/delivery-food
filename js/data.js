24ff3af7970ea4a47b9db16ddc997ec84d273f62
Anna <anna.ne4ik@gmail.com>
2026-01-09 21:12:12 +0200

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const restaurantsData = [
+  {
+    name: 'Піца плюс',
+    time: '50 хвилин',
+    rating: '4.5',
+    price: 200,
+    category: 'Піца',
+    image: 'img/pizza-plus/preview.jpg'
+  },
+  {
+    name: 'Танукі',
+    time: '60 хвилин',
+    rating: '4.5',
+    price: 1200,
+    category: 'Суші, роли',
+    image: 'img/tanuki/preview.jpg'
+  },
+  {
+    name: 'FoodBand',
+    time: '40 хвилин',
+    rating: '4.5',
+    price: 150,
+    category: 'Піца',
+    image: 'img/food-band/preview.jpg'
+  },
+  {
+    name: 'Ikigai',
+    time: '55 хвилин',
+    rating: '4.5',
+    price: 250,
+    category: 'Піца',
+    image: 'img/palki-skalki/preview.jpg'
+  },
+  {
+    name: 'Пузата хата',
+    time: '75 хвилин',
+    rating: '4.5',
+    price: 300,
+    category: 'Українські страви',
+    image: 'img/gusi-lebedi/preview.jpg'
+  },
+  {
+    name: 'PizzaBurger',
+    time: '45 хвилин',
+    rating: '4.5',
+    price: 700,
+    category: 'Піца',
+    image: 'img/pizza-burger/preview.jpg'
+  }
+];
